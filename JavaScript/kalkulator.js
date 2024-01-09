02b7a6867e7a5b91908df00ad2923a5315c73964
zsooofi <meleghzsofia11@gmail.com>
2024-01-09 16:44:43 +0100

    :)

--- JavaScript/kalkulator.js
+++ JavaScript/kalkulator.js
@@ -1,4 +1,16 @@
 function fontKilogrammKonverter (valNUM) 
 {
-document.getElementById("outputKilogramm").innerHTML = valNUM * 0.453592;
+document.getElementById("outputKilogramm").innerHTML = (valNUM * 0.453592).toFixed(3);
+}
+
+function kilogrammFontKonverter (valNUM) {
+    document.getElementById("outputFont").innerHTML = (valNUM * 2.204623).toFixed(3);
+}
+
+function round (number, precision) {
+    multiplier = Math.pow (10, precision)
+    number = number*multiplier
+    Math.round(number)
+    number = number/multiplier;
+    return number;
 }
